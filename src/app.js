b4ddb7e301ac53857614ebce0e7d088c2dc2f1a8
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-12-10 19:06:09 +0100

    Added contact form for the pages

--- src/app.js
+++ src/app.js
@@ -37,3 +37,45 @@ window.onscroll = () => {
 
 const footer = document.querySelector(".footer");
 footer.innerHTML = footerHTML;
+
+/* ############################################################
+--- <<<     Form Section     >>> ---
+---############################################################ */
+const contactForm = document.querySelector("#contactForm");
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const passedValidation = document.querySelector("#passedValidation");
+
+contactForm.addEventListener("submit", contactFormHandler);
+
+function contactFormHandler(event) {
+  event.preventDefault();
+
+  subject.value.trim().length >= 10
+    ? (subjectError.style.display = "none")
+    : (subjectError.style.display = "block");
+
+  message.value.trim().length >= 25
+    ? (messageError.style.display = "none")
+    : (messageError.style.display = "block");
+
+  validateEmail(email.value)
+    ? (emailError.style.display = "none")
+    : (emailError.style.display = "block");
+
+  subject.value.trim().length >= 10 === true &&
+  message.value.trim().length >= 25 === true &&
+  validateEmail(email.value) === true
+    ? (passedValidation.style.display = "block")
+    : (passedValidation.style.display = "none");
+}
+
+const validateEmail = (email) => /\S+@\S+\.\S+/.test(email);
+
+const validateSubject = (subject, n) => {
+  return subject.value.trim().length >= n
+    ? (subjectError.style.display = "none")
+    : (subjectError.style.display = "block");
+};
